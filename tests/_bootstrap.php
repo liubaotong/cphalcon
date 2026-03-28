896932cb4b960a177126317e8a34520de0a9e95c
Nikolaos Dimopoulos <nikos@niden.net>
2026-03-28 09:01:40 -0500

    trying dotenv

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -7,6 +7,11 @@ define('PROJECT_PATH', $root);
 
 require_once $root . 'tests/_config/functions.php';
 
+if (file_exists(rootDir('.env'))) {
+    $dotenv = Dotenv\Dotenv::createImmutable(rootDir());
+    $dotenv->load();
+}
+
 loadIni();
 loadFolders();
 loadDefined();
