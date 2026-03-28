b47131cd0af180c9144569aaacfc9316d56ec74f
Nikolaos Dimopoulos <nikos@niden.net>
2026-03-28 08:14:53 -0500

    forms to phpunit

---
+++ tests/unit/Forms/Element/GetSetFiltersTest.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Forms\Element;
+
+use Phalcon\Forms\Exception;
+use Phalcon\Tests\Fixtures\Traits\FormsTrait;
+use Phalcon\Tests\AbstractUnitTestCase;
+
+use function array_flip;
+use function uniqid;
+
+final class GetSetFiltersTest extends AbstractUnitTestCase
+{
+    use FormsTrait;
+
+    /**
+     * Tests Phalcon\Forms\Element\* :: getFilters()/setFilters()/addFilter()
+     *
+     * @dataProvider getExamples
+     *
+     * @return void
+     *
+     * @author       Phalcon Team <team@phalcon.io>
+     * @since        2021-12-05
+     */
+    public function testFormsElementGetSetAddFilters(
+        string $class
+    ): void {
+        $name    = uniqid();
+        $data    = [
+            'trim',
+            'striptags',
+        ];
+        $flipped = array_flip($data);
+        $object  = new $class($name);
+
+        $expected = [];
+        $actual   = $object->getFilters();
+        $this->assertSame($expected, $actual);
+
+        $object->setFilters($flipped);
+
+        $expected = $flipped;
+        $actual   = $object->getFilters();
+        $this->assertSame($expected, $actual);
+
+        $object->setFilters('lower');
+
+        $expected = ['lower'];
+        $actual   = $object->getFilters();
+        $this->assertSame($expected, $actual);
+
+        $object->addFilter('trim');
+
+        $expected = ['lower', 'trim'];
+        $actual   = $object->getFilters();
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * Tests Phalcon\Forms\Element\* :: setFilters() - exception
+     *
+     * @dataProvider getExamples
+     *
+     * @return void
+     *
+     * @author       Phalcon Team <team@phalcon.io>
+     * @since        2021-12-05
+     */
+    public function testFormsElementSetFiltersException(
+        string $class
+    ): void {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('The filter needs to be an array or string');
+
+        $object = new $class(uniqid());
+        $object->setFilters(1234);
+    }
+}
