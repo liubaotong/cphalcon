b3e7368a9cd64a0c40ef4b6de3f03e9d8464f8f9
Nikolaos Dimopoulos <nikos@niden.net>
2026-03-27 19:16:51 -0500

    filter, flash, forms, html, http to phpunit

---
+++ tests/unit/Http/Request/GetServerAddressTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of the Phalcon Framework.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Tests\Unit\Http\Request;
+
+use Phalcon\Tests\Fixtures\Page\Http;
+use Phalcon\Tests\Unit\Http\Helper\AbstractHttpBase;
+
+use function gethostbyname;
+
+final class GetServerAddressTest extends AbstractHttpBase
+{
+    /**
+     * Tests getServerAddress
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2014-10-04
+     */
+    public function testHttpRequestGetServerAddress(): void
+    {
+        $request = $this->getRequestObject();
+
+        $_SERVER['SERVER_ADDR'] = Http::TEST_IP_ONE;
+
+        $expected = Http::TEST_IP_ONE;
+        $actual   = $request->getServerAddress();
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * Tests getServerAddress default
+     *
+     * @author Phalcon Team <team@phalcon.io>
+     * @since  2014-10-04
+     */
+    public function testHttpRequestGetServerAddressDefault(): void
+    {
+        $request = $this->getRequestObject();
+
+        $expected = gethostbyname('localhost');
+        $actual   = $request->getServerAddress();
+        $this->assertSame($expected, $actual);
+    }
+}
